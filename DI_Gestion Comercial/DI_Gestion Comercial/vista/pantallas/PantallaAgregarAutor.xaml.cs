d2aa32c16e932d6c6b49004e15b3d2c9e00b82fe
lausnchez <lausnchezmartn@gmail.com>
2025-03-06 20:48:40 +0100

    Detalles pequeños de la pantalla principal

---
+++ DI_Gestion Comercial/DI_Gestion Comercial/vista/pantallas/PantallaAgregarAutor.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DI_Gestion_Comercial.vista.pantallas
+{
+    /// <summary>
+    /// Lógica de interacción para PantallaAgregarAutor.xaml
+    /// </summary>
+    public partial class PantallaAgregarAutor : Window
+    {
+        public PantallaAgregarAutor()
+        {
+            InitializeComponent();
+        }
+    }
+}
